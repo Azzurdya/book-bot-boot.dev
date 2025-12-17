0d01d03288e68fce876a4fadc9c7ab0d4b5bf888
Azzurdya <suriraaditya08@gmail.com>
2025-12-17 13:07:24 +1300

    add new branch get_book_to_python add code to main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print ("greetings boots")
+with open(/Users/as142/desktop/projects/boot.dev/book_bot/book-bot-boot.dev/books/frankenstein.txt) as f:
+    books_string = f.read()
+    print (books_string)
