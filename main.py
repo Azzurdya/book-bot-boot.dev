fea93675ac619be9a96b3c62f4713294df521deb
Azzurdya <suriraaditya08@gmail.com>
2025-12-18 08:37:19 +1300

    add split_count branch

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from sre_parse import RANGE
+
+
 def get_book_txt(file_path):
     book = ""
     with open(file_path) as f:
         book = f.read()
         print(book)
     return book
+
+
+def split_count(book_string):
+    book_string = book_string.split()
+    for book_string in RANGE(1, 10):
+        print(book_string)
 
 
 def main():
